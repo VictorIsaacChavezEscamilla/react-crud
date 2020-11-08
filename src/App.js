8e4cc99fe23420cc3b3a5d41d4a579e3bf18dad9
Victor Isaac Chavez Escamilla <victorisaac.chavez@hotmail.com>
2020-11-08 11:05:55 -0600

    tailwindcss basic initialization and include Navbar third party component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+
+import { TailwindNavbar } from "tailwind-navbar-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TailwindNavbar
+      brand={<img src={logo} width="40" height="40" alt="Brand logo" />}
+      className="py-1">
+      <nav>
+        <ul className="items-center justify-between pt-4 text-base lg:flex lg:pt-0">
+          <li>
+            <a
+              className="block px-0 py-3 border-b-2 border-transparent lg:p-4 hover:border-indigo-400"
+              href="/">
+              A Link
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </TailwindNavbar>
   );
 }
 
